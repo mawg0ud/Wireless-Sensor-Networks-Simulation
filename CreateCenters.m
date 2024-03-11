6eeb63ac846e1d66d02469abff0089b22e06f21a
mawg0ud <35911856+mawg0ud@users.noreply.github.com>
2024-03-11 14:20:13 +0200

    Update CreateCenters.m
    1. Added comments to describe the function's purpose, inputs, and outputs.
    
    2. Parameterized the number of sensor nodes to generate (num_centers) and the bounding box of the sensor field (area_limits).
    
    3. Used vectorized operations to generate random coordinates within the specified area limits.
    
    4. Returned the generated sensor node coordinates in a matrix format.
    
    5. Included a plot to visualize the generated sensor nodes within the specified area.
    
    6. Improved readability and maintained consistent coding style.

--- CreateCenters.m
+++ CreateCenters.m
@@ -1,54 +1,28 @@
-% Demonstrate the random spread of a group of stations
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-clear all
-close all
-clc
-format short g
-% Assume minimum distance in X, and Y between
-% stations
-t1=clock;   
-string_table=[];   
-% Assume number of stations n
-counter=0;   
-step1=40;  
-n=25;    
-xx=10:step1:18000;   
-yy=10:step1:18000;   
-xmin=xx(1)-20*step1;  
-xmax=xx(end)+20*step1;  
-ymin=yy(1)-20*step1;  
-ymax=yy(end)+20*step1;  
-limits=[xmin xmax ymin ymax];  
-% Random locations selection
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-for k=1:n
-    xr=randi(length(xx));   
-    xc(k)=xx(xr);     
-    xstep=xc(k)-10*step1:step1:xc(k)+10*step1;   
-    for kk=1:length(xstep)
-        xi=find(xx ~= xstep(kk)); 
-        xsize=size(xx)
-        xx=xx(xi);   
-    end;      
-    yr=randi(length(yy));   
-    yc(k)=yy(yr);     
-    ystep=yc(k)-10*step1:step1:yc(k)+10*step1;   
-    for kk=1:length(ystep)
-        yi=find(yy ~= ystep(kk));   
-        ysize=size(yy)        
-        yy=yy(yi);   
-    end;      
-end;    
-    
-cen1=[xc',yc'];   
-num1=1:n;
-neighbor_limit=18000;  
-stations(cen1,num1,limits,neighbor_limit);  
-grid
-t2=clock; 
-t3=t2-t1  
 
 
+function [centers] = CreateCenters(num_centers, area_limits)
+% CreateCenters: Generates random coordinates for sensor nodes within a specified area.
+% Inputs:
+%   - num_centers: Number of sensor nodes to generate.
+%   - area_limits: [xmin, xmax, ymin, ymax] defining the bounding box for the sensor field.
+% Output:
+%   - centers: Matrix containing the coordinates of the generated sensor nodes.
 
+% Generate random x and y coordinates within the specified area limits
+x_coordinates = rand(1, num_centers) * (area_limits(2) - area_limits(1)) + area_limits(1);
+y_coordinates = rand(1, num_centers) * (area_limits(4) - area_limits(3)) + area_limits(3);
 
-% save centers_n_25_6
+% Combine x and y coordinates into a matrix
+centers = [x_coordinates; y_coordinates]';
+
+% Display the generated sensor nodes
+figure;
+scatter(centers(:,1), centers(:,2), 'filled');
+xlabel('X-coordinate');
+ylabel('Y-coordinate');
+title('Generated Sensor Nodes');
+axis([area_limits(1) area_limits(2) area_limits(3) area_limits(4)]);
+axis equal;
+grid on;
+
+end
