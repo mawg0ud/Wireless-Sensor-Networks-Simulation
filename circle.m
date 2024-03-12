a8b24afe5a7e5fbd6930a7fc56999651c8340854
mawg0ud <35911856+mawg0ud@users.noreply.github.com>
2024-03-12 15:00:49 +0200

    Update circle.m
    Changes made:
    
    1- Added comments to describe the function's purpose, inputs, and outputs.
    2- Used meaningful variable names to improve code readability.
    3- Added an optional input parameter num_points to specify the number of points to generate on the circle.
    4- Utilized MATLAB's linspace function to generate equally spaced points along the circle.
    5- Removed unnecessary code and redundant calculations.

--- circle.m
+++ circle.m
@@ -1,13 +1,19 @@
-function [xc,yc]=circle(cen,radius)   
-% function [xc,yc]=circle(cen,radius);   
-% xc a vector of x coordinates ...
-% yc a vector of y coordinates ...
-% cen is the center of the circle
-% radius of the circle
-th1=0:.01:2*pi;   
-xc=radius*cos(th1);   
-yc=radius*sin(th1);   
-xc = xc + cen(1);  
-yc = yc + cen(2);  
+function [x, y] = circle(center, radius, num_points)
+% circle: Generates points on a circle.
+% Inputs:
+%   - center: Coordinates of the center of the circle [x, y].
+%   - radius: Radius of the circle.
+%   - num_points: Number of points to generate (optional, default is 100).
+% Outputs:
+%   - x: x-coordinates of the points on the circle.
+%   - y: y-coordinates of the points on the circle.
 
+if nargin < 3
+    num_points = 100; % Default number of points
+end
 
+theta = linspace(0, 2*pi, num_points);
+x = center(1) + radius * cos(theta);
+y = center(2) + radius * sin(theta);
+
+end
