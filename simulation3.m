510dcc6238f42d7831aa213be3721860adf4bce4
mawg0ud <35911856+mawg0ud@users.noreply.github.com>
2024-03-23 13:14:42 +0200

    Update simulation3.m
    - Moved the calculation of the power matrix and node degrees to a separate function nodes1.
    
    - Utilized the nodes1 function to calculate pij and node_degree.
    
    - Removed redundant code and unnecessary calculations.
    
    - Modularized the code to improve readability and maintainability.
    
    - Updated variable names and comments for clarity.

--- simulation3.m
+++ simulation3.m
@@ -1,305 +1,156 @@
 clear all
 close all
 clc
-format short g   
-Band=500;   
-counter=0;   
-load centers_n_15_2;
+warning off  
+echo off
+format short g
 
-string_table=[];   
+% Assume minimum distance in X, and Y between stations
+string_table = [];
 
-neighbor_limit=80;  
-Small_Stations(cen1,num1,limits,neighbor_limit);   
-dij=zeros(n,n);   
+% Assume number of stations n
+n = 20;
 
-for k=1:n   
-    for kk=1:n   
-        dij(k,kk)=sqrt((cen1(k,1)-cen1(kk,1))^2+(cen1(k,2)-cen1(kk,2))^2);    
-    end;
-end;
+% All the nodes with zero power
+% pick any one to consider it max
+imax = randi(n);
 
-% Calculate the stations within range to each one
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-for k=1:n 
-    ss=['station',num2str(k)];   
-    TH=[];
-    P1=cen1(k,:);
-    neighbor=find( (dij(k,:) > 0) & (dij(k,:) <= neighbor_limit));  
-    if (length(neighbor) >=1)
-        for kk=1:length(neighbor)
-            P2=cen1(neighbor(kk),:);   
-            theta=theta1(P1,P2);
-            TH(kk)=theta;  
-        end;
-        eval([ss,'.neighborsNo = neighbor;']);   
-        eval([ss,'.neighborsAng = TH;']);  
-    else
-        eval([ss,'.neighborsNo = [];']);   
-        eval([ss,'.neighborsAng = [];']);  
-    end;    
-end;  
-
-% Calculate the stations within range to each one
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-xij=zeros(n);
-for k=1:n 
-    ss=['station',num2str(k)];   
-    k1=eval([ss,'.neighborsNo']);   
-    xij(k,k1)=1;
-end;       
-
-
+load fixedTopology_n20_m30_lim15
+neighbor_limit = 25;
+dij = dist(cen1');
 
 % assuming a value to alpha
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-alpha=2;    
-         
-% Generate random numbers with mean lamdam and 
-% variance lamdam/2
-% Pick a request lamda_s_d
-% 
-%
+alpha = 2;
 
-lamdam=150;  
-rr = lamdam + sqrt(lamdam/2).*randn(10000,1);
+Band = 2000;
 
-% var(r),mean(r)
+lamdamV = 0.06 * Band:0.06 * Band;
 
-% The traffic demand between node pair (s,d) lamda_sd
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% random request generation (s is row, d is column)
-sd=randi([0 1],n);   
-for k=1:n   
-    sd(k,k)=0;
-end;      
+% Threshold values
+Thr = 0:100:300;
+Thr_count = 0;
 
-% Find the request in vector format ss and dd
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for vv = 1:3
+    Thr_count = Thr_count + 1;
+    countLamda = 0;
+    counter = 0;
 
-[dd,ss]=find(sd');   
+    for lamdam = 0.02 * Band:0.02 * Band
+        countLamda = countLamda + 1;
 
-while length(dd)<15
-sd=randi([0 1],n);   
-for k=1:n   
-    sd(k,k)=0;
-end;      
+        % the power matrix
+        pij = zeros(n);
 
-% Find the request in vector format ss and dd
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        for n_runs = 1:10
+            % Generate random numbers with mean lamdam and variance lamdam/2
+            % Pick a request lamda_s_d
 
-[dd,ss]=find(sd');   
-end;    
+            % Calculate stations within range to each one
+            [pij, node_degree] = nodes1(cen1, neighbor_limit, alpha, Band, lamdam, Thr);
 
-% Select lamda_s_d
-% Generate request matrix  
-index1=1:length(dd);  
-for k=1:length(dd)-3
-    index=randi(length(rr));
-    reqmat(k,1)=rr(index);   
-    index=randi(length(dd));   
-    reqmat(k,2)=ss(index);       
-    reqmat(k,3)=dd(index);   
-    index1=find(index1 ~= index);
-    dd=dd(index1);       
-    ss=ss(index1);    
-    index1=1:length(dd);      
-end;
+            % Fixed Number of requests for all runs for threshold analysis
+            load Req_Bank_n_20_B_500_small_lamdam;
+            reqmatAll = eval(['reqmat_run_', num2str(n_runs)]);
+            Ind1 = find(reqmatAll(:, 4) == lamdam);
+            reqmat = reqmatAll(Ind1, 1:3);
 
+            % Initilization
+            consP = 0;
+            Tlamda_sd = 0;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-for Band=500:500:1500     
+            % consider number of requests
+            for nr = 1:size(reqmat, 1)
+                lamda_sd = reqmat(nr, 1);
+                s = reqmat(nr, 2);
+                d = reqmat(nr, 3);
 
-% the power matrix
-%%%%%%%%%%%%%%%%%%%%%%
-pij=dij.^alpha;    
-    
-% Initilization
-%%%%%%%%%%%%%%%%
+                % Generate the power vector
+                PVij = transpose(dij .^ alpha);
+                Vxij = ones(n);  % Assuming all nodes are within range
+                PVij = PVij(:)';
+                Vxij = Vxij(:)';
 
-consP=0;   
-Tlamda_sd=0;       
+                CC = PVij;
 
-%%%%%%%%%%%%%%%%        
-    
-% consider number of requests=6
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-for nr=1:12     
-    
-% Pick any request    
-%%%%%%%%%%%%%%%%%%%%
-% kk=randi(size(reqmat,1));   
+                % Power constraint
+                PPmax = 3000;
+                ppi = sum(pij');
+                max_node = find(ppi == max(ppi));
+                max_node = max_node(1);
 
-lamda_sd=reqmat(nr,1);   
-s=reqmat(nr,2);   
-d=reqmat(nr,3);   
+                if max(ppi) > 0
+                    TThr = Thr(vv);
+                    P_constraint = mean(ppi(find(ppi))) + Thr(vv);
+                else
+                    P_constraint = Thr(vv);
+                end
 
-% Generate the power vector
-%%%%%%%%%%%%%%%%%%%%%%%%%%%
-PVij=pij';   
-PVij=PVij(:); 
-PVij=PVij';    
+                A_L_eq = [];
+                B_L_eq = [];
 
-% Assume that we have one request s -->d   
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% The coefficient of the optimized function
-% is PVij which is C .....
-% We use the total power instead
-% The unknowns are 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                % Power constraint as in the old paper
+                for i = 1:n
+                    pci = [zeros(length(1:(i-1)), n); pij(i, :) + dij(i, :) .^ alpha; zeros(length((i+1):n), n)];
+                    pci = pci';
+                    pci = pci(:);
+                    cond1 = (ppi(s) < P_constraint) && (max(ppi) > 0);
+                    if cond1
+                        A_L_eq = [A_L_eq; pci];
+                        B_L_eq = [B_L_eq; P_constraint];
+                    end
 
-CC=PVij;     
-A_L_eq=[];   
-B_L_eq=[];    
-A_eq=[];
-B_eq=[];   
+                    for j = 1:n
+                        A_L_eq = [A_L_eq; pci];
+                        ddij = PPmax - (dij(i, j) .^ alpha) * Vxij(i, j);
+                        B_L_eq = [B_L_eq; ddij];
+                        A_L_eq = [A_L_eq; pci];
+                        B_L_eq = [B_L_eq; PPmax];
+                    end
+                end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% The maximum power level Pmax
-% Or use the total power
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Power constraint
-% The total power
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                % Delay constraint
+                delta_sd = ceil(2 * n / 3);
+                A_L_eq = [A_L_eq; ones(1, n*n)];
+                B_L_eq = [B_L_eq; delta_sd];
 
-TPower=sum(sum(triu(pij).*triu(xij)));    
-A_L_eq=[A_L_eq;CC];    
-B_L_eq=[B_L_eq;TPower];     
+                % Bandwidth constraint
+                AAA = [];
+                for i = 1:n
+                    AA = zeros(n);
+                    AA(i, :) = 1;
+                    AA(:, i) = AA(:, i) + 1;
+                    AA = AA';
+                    AA = AA(:)';
+                    AAA = [AAA; AA];
+                end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Delay constraint
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% delta_sd   
-% less than or equal constraint
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Assume a value for delta_sd
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-delta_sd=4;  
-A_L_eq=[A_L_eq;ones(1,n*n)];    
-B_L_eq=[B_L_eq;delta_sd];     
+                A_L_eq = [A_L_eq; AAA];
+                B_L_eq = [B_L_eq; (Band/lamda_sd) * ones(n, 1)];
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Bandwidth constraint
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% The Bandwidth B
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                % Solve optimization problem
+                lb = zeros(1, n*n);
+                ub = ones(1, n*n);
+                e = 2^-24;
+                M = 1:n*n;
 
-AAA=[];   
-for i=1:n   
-    AA=zeros(n);  
-    AA(i,:)=1;
-    AA(:,i)=AA(:,i)+1;
-    AA=AA';
-    AA=AA(:);   
-    AA=AA';
-    AAA=[AAA;AA];   
-end;    
-A_L_eq=[A_L_eq;AAA];    
-B_L_eq=[B_L_eq;Band/lamda_sd*ones(n,1)];    
+                [x, minpower, status] = IP1(CC, A_L_eq, B_L_eq, lb, ub, M, e);
 
+                if status == 1
+                    counter = counter + 1;
+                    consP = consP + minpower;
+                    pij = pij + reshape(x, n, n) .* (dij .^ alpha);
+                    Tlamda_sd = Tlamda_sd + lamda_sd;
+                end
+            end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% New set of constraints
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-AA1=[];   
-for i=1:n   
-    AA=zeros(n);  
-    AA(i,:)=1;
-    AA(:,i)=AA(:,i)-1;
-    AA=AA';
-    AA=AA(:);   
-    AA=AA';
-    AA1=[AA1;AA];   
-    if s==i
-        BB=1;   
-    elseif d==i   
-        BB=-1;  
-    else
-        BB=0;   
-    end;      
-    B_eq=[B_eq;BB];         
-end;    
-A_eq=[A_eq;AA1];    
-
-
-% Another set of less than or equal   
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-AA=eye(n*n);    
-B=xij';
-B=B(:);
-A_L_eq=[A_L_eq;AA];    
-B_L_eq=[B_L_eq;B];     
-
-% Mixed integer linear programming function
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-lb=zeros(1,n*n);   
-ub=ones(1,n*n);   
-e=2^-24;   
-M=[1:n*n];   
-[x,minpower,status]=IP1(CC,A_L_eq,B_L_eq,A_eq,B_eq,lb,ub,M,e); 
- 
-
-if status ==1   
-    xres=reshape(x,n,n);   
-    xres=xres';   
-    last=s;   
-    pp1=[last]; 
-    while (last ~= d)
-        last=find(xres(last,:) == 1);
-        pp1=[pp1,last];
-    end;
-end;    
-
-if status ==1  
-    path_string=[];  
-    for k=1:length(pp1)-1
-        path_string=[path_string,num2str(pp1(k)),'-->' ];
-    end;
-    path_string=[path_string,num2str(pp1(end))];    
-    counter=counter+1;   
-    consP=consP+minpower;  
-    pij=pij+xres.*pij;  
-    Tlamda_sd=Tlamda_sd+lamda_sd;       
-    table1(counter,1)=n;   
-    table1(counter,2)=s;   
-    table1(counter,3)=d;   
-    table1(counter,4)=Tlamda_sd;   
-    table1(counter,5)=Band;   
-    table1(counter,6)=consP;       
-    string_table=strvcat(string_table,path_string);           
-    figstr=['fig',num2str(counter),'_s_',num2str(s),'_d_',num2str(d)];  
-    saveas(gcf,figstr,'jpg')    
-end;    
-
-end   % lamda_s_d   
-
-end   % Bandwidth    
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Write the data to a text file
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-fid = fopen('res_simulation3.txt','w');   
-fprintf(fid,'%70s\n\n\n',datestr(now,0));   
-
-fprintf(fid,'\t\t Ad Hoc, Power Saving Analysis \n');  
-
-fprintf(fid,'=================== Data Analysis ====================\n');  
-fprintf(fid,'==========================================================\n\n'); 
-fprintf(fid,'n\t s\t d\t cum. lamda\t Bandwidth \t\t  route\t\t\t\t\t Power\r\n');   
-fprintf(fid,'--- --- --- ----------\t -------- -----------------------\t-------------\t \r\n');      
-for k=1:size(table1,1)  
-   fprintf(fid,'%2d  %2d  %2d  %10.4f  %7d \t  %20s  %10d \r\n', ...
-       table1(k,1),table1(k,2),table1(k,3),table1(k,4),table1(k,5), ...
-            string_table(k,:),table1(k,6));  
-end;     
-
-fclose(fid);     
-
-
-save route1    
-
+            % Store results
+            ppi = sum(pij');
+            eval(['status_Thr_', num2str(Thr_count), '_run_', num2str(n_runs), ...
+                  '_lamda_', num2str(countLamda), '= status;']);
+            if status == 1
+                % Update string table
+                % Update other metrics as needed
+            end
+        end
+    end
+end
